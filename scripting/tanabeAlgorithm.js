0cbc254125911741721d0889c8690c5911da8df4
Arnav Kumar <32623368+arnavcs@users.noreply.github.com>
2020-05-22 11:02:00 -0600

    Added the way to calculate the number of reference and query alleles
    This is incomplete as of now. We still need to add the way to calculate the number of shared alleles.

--- scripting/tanabeAlgorithm.js
+++ scripting/tanabeAlgorithm.js
@@ -3,11 +3,29 @@
 // This function takes in the parameters for a tanabe-style reference and returns the concordance amount.
 // The mode is an int with 0 being non-empty markers, 1 being query markers, and 2 being reference markers.
 function tanabe(reference, query, mode, amelogenin = true) {
-  let numSharedAlleles = 0;
   
   //calculates the number of reference and query alleles.
+  let numReferenceAlleles = 0;
+  let numQueryAlleles = 0;
   
+  //calculates the number of alleles in the reference
+  for (let location of reference.loci) {
+    numReferenceAlleles += location.length;
+  }
+  if (!amelogenin) {
+    numReferenceAlleles -= reference.loci.Amelogenin.length; //removes the extra 
+  }
   
+  //calculates the number of alleles in the query
+  for (let location of query.loci) {
+    numQueryAlleles += location.length;
+  }
+  if (!amelogenin) {
+    numQueryAlleles -= query.loci.Amelogenin.length; //removes the extra 
+  }
+   
+  //calculates the number of shared alleles.
+  let numSharedAlleles = 0;
   if (mode == 0) {
     //non-empty marker case
     
