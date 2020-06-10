9ca33e6bd1dc603417d20fd608f1ba22cdd53058
KaiElrick <59662211+KaiElrick@users.noreply.github.com>
2020-06-10 17:05:37 -0600

    Create settingsGrab.js

---
+++ scripting/settingsGrab.js
@@ -0,0 +1,34 @@
+window.onload = () => {
+    let settings = {};
+    document.getElementById("compareButton").addEventListener("click",() => {settings = settingsGrab()});
+}
+function settingsGrab(){
+    let settings = {}
+    //modes 
+    if(document.getElementById("nonEmptySelect").checked){
+        settings.mode = 0;
+    }
+    else if(document.getElementById("querySelect").checked){
+        settings.mode = 1;
+    }
+    else if(document.getElementById("referenceSelect").checked){
+        settings.mode = 2;
+    }
+    //algorithm
+    if(document.getElementById("tanabeSelect").checked){
+        settings.algorithm = 0;
+    }
+    else if(document.getElementById("mastersVQuerySelect").checked){
+        settings.algorithm = 1;
+    }
+    else if(document.getElementById("mastersVRefernceSelect").checked){
+        settings.algorithm = 2;
+    }
+    if(document.getElementById("includeAmelogeninInput").checked){
+        settings.amelogenin = true;
+    }
+    else{
+        settings.amelogenin = false;
+    }
+    return settings;
+}
